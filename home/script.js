41627ddab7ff529a571882468a33ab1fb21fd31b
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2021-11-07 10:41:40 -0800

    its been 84years since ive made a public repo

---
+++ home/script.js
@@ -0,0 +1,16 @@
+let username = "Jonas Kahnwald"
+const  username = "Jonas Kahnwald"
+
+// const cannot be overwritten vs. let which can //
+
+// NAMING A VARIABLE -- naming is a process
+
+// BAD
+let cid = 12; //wtf is a `cid`
+// GOOD
+let clientID = 12; // ah it's a client id
+
+// BAD
+let id = 12; // what id is this? a user? a client? a cat?
+// GOOD
+let userID = 12; // be specific
